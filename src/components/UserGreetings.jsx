d49f8038f572795d64dc521f01d351d68985db1f
Deepsikha Pradhan <deepsikha.rk@hutechsolutions.in>
2024-10-13 23:12:10 +0530

    oct-11 pending changes

--- src/components/UserGreetings.jsx
+++ src/components/UserGreetings.jsx
@@ -9,18 +9,30 @@ import React, { Component } from 'react'
       }
     }
   render() {
-    if (this.state.isLoggedIn) {
-        return (
-            <div>
-                Welcome User
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                Welcome Guest
-            </div>
-        )
+    return (
+      <div> {this.state.isLoggedIn ? <div> Welcome User </div> : <div> Welcome Guest </div>} </div>
+
+    )
+    //return(
+     // let message 
+     // if (this.state.isLogggedIn) {
+     //   message = <div>Welcome User</div>
+     // } else {
+     //   message = <div>Welcome Guest</div>
+     // }
+    //)
+    //if (this.state.isLoggedIn) {
+       // return (
+          //  <div>
+           //     Welcome User
+           // </div>
+      //  )
+    //} else {
+        //return (
+          //  <div>
+          //      Welcome Guest
+          //  </div>
+       // )
     }
    // return (
      //   <div>
@@ -32,6 +44,6 @@ import React, { Component } from 'react'
       
     //)
   }
-}
+
 
 export default UserGreetings
